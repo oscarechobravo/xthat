328c1179e9384feb43227aa86363d707e723ad95
Oliver <oliver.bates@gmail.com>
2026-08-12 16:07:25 +0100

    adding instructions and a title!

--- xthat.py
+++ xthat.py
@@ -17,18 +17,26 @@ def save_prompt(prompt: str, response: str):
     #write the line to file in a tuple of {name:prompt}
     #close - maybe print writing successful, or there was an error
     with open(OUTFILE, "a") as f:
+        ###check file exists and create a csv if not
+
+        #write to the file
         f.write("\""+prompt+"\",\""+response+"\"")
     print("Response recorded.")
         
 
 def main():
+    print(text2art("Welcome \nto \nxThat."))
+    print("xThat is a playful mini game generator for anyone looking to create their own mini game.\n\nTo make a game we will:\n1. provide you with a title for your game\n2. prompt you for a subtitle\n3. prompt you for 4 bullet points for game play.")
+    #ask if they want an example - then show them screw that.
     line_list = load_lines(DICNAME)
     prompt = line_list[random.randrange(len(line_list)-1)].upper()
     print(text2art(prompt + " THAT", font="small"))
+    print("Enter a subtitle")
+    subtitle = input()
     print("Enter a prompt")
     response = input()
     print("You said: " + response)
-    save_prompt(prompt, response)
+    save_prompt(prompt, subtitle, response)
 
 
 
