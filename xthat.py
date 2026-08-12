03e96ba38220fbc3b5f80f05edb6d6101783ca1a
Oliver <oliver.bates@gmail.com>
2026-08-12 15:55:04 +0100

    print the prompt and that you've written to file

--- xthat.py
+++ xthat.py
@@ -3,23 +3,30 @@ import random
 from pathlib import Path
 
 DICNAME = "xthat.dic"
+OUTFILE = "log.txt"
 
 def load_lines(filename: str):
     with open(filename) as f:
         your_list = f.read().splitlines()
     return your_list
 
-def save_prompt(prompt: str):
+def save_prompt(prompt: str, response: str):
     #try and open file, create it if it doesn't exist
     #write the line to file in a tuple of {name:prompt}
     #close - maybe print writing successful, or there was an error
+    with open(OUTFILE, "a") as f:
+        f.write("\""+prompt+"\",\""+response+"\"")
+    print("Response recorded.")
+        
 
 def main():
     line_list = load_lines(DICNAME)
-    print(line_list[random.randrange(len(line_list)-1)].upper() + " THAT")
+    prompt = line_list[random.randrange(len(line_list)-1)].upper()
+    print(prompt + " THAT")
     print("Enter a prompt")
-    prompt = input()
-    print("You said: " + prompt)
+    response = input()
+    print("You said: " + response)
+    save_prompt(prompt, response)
 
 
 
